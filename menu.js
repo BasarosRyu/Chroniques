57f81fb1a14ba828a3d50027bacb30bc92cad6d9
Basaros <42807430+BasarosRyu@users.noreply.github.com>
2021-07-24 19:22:05 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  /* On surveille les cliques sur les liens du menu */
+  $("#list li").click(function(e) {
+    /* On désactive l'action par défaut des liens */
+    e.preventDefault();
+
+    $($(this).next('ul').children().toggleClass('displayLI'));
+    $($(this).next('ul').children().toggleClass('displayDefault'));
+  });
+
+
+  $('.sub-ul li').click(function(e) {
+    e.preventDefault();
+
+    $('#chronique-container').load($(this).data("id") + ".html");
+  })
+});
